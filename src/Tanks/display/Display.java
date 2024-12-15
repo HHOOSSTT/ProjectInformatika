737d521c9e266f777f4065a6637a97c61e9209d3
HHOOSSTT <milovanarsen@gmail.com>
2024-12-15 21:24:31 +0300

    движение вправо и влево

--- src/Tanks/display/Display.java
+++ src/Tanks/display/Display.java
@@ -127,19 +127,20 @@ public abstract class Display {
         public void keyPressed(KeyEvent e) {
             int f = e.getKeyCode();
             if((f == KeyEvent.VK_W) && (game)) {
-                if((((tankx==775)||(tankx==125)||(tankx==325)||(tankx==575))&&(tanky>=125))||(((tankx==150)||(tankx==350)||(tankx==550)||(tankx==750))&&((tanky>175)&&(tanky<=425)))||(((tankx==175)||(tankx==725))&&(((tanky<=525)&&(tanky>=400))||((tanky>=125)&&(tanky<=275))))||(((tankx==200)||(tankx==250)||(tankx==300)||(tankx==600)||(tankx==650)||(tankx==700))&&(((tanky>=200)&&(tanky<=225))||((tanky<=475)&&(tanky>=400))))||((tankx==225)&&(((tanky>=125)&&(tanky<=225))||((tanky<=525)&&(tanky>=400))))||(((tankx==375)||(tankx==525))&&(((tanky<=525)&&(tanky>=425))||((tanky>=125)&&(tanky<=300))))||(((tankx==400)||(tankx==500))&&((tanky==225)||(tanky==300)||(tanky==425)))||(((tankx==425)||(tankx==475))&&((tanky!=250)&&(tanky!=275)&&(tanky!=350)&&(tanky!=375)&&(tanky>=125)))||((tankx==450)&&((tanky==425)||(tanky==400)||(tanky==325)||(tanky==300)||(tanky==225)))){
+                if(((((tankx==775)||(tankx==125)||(tankx==325)||(tankx==575))&&((tanky>=125)&&(tanky<=525))||(((tankx==150)||(tankx==350)||(tankx==550)||(tankx==750))&&((tanky>175)&&(tanky<=425)))||(((tankx==175)||(tankx==725))&&(((tanky<=525)&&(tanky>=400))||((tanky>=125)&&(tanky<=275))))||(((tankx==200)||(tankx==250)||(tankx==300)||(tankx==600)||(tankx==650)||(tankx==700))&&(((tanky>=200)&&(tanky<=225))||((tanky<=475)&&(tanky>=400))))||((tankx==225)&&(((tanky>=125)&&(tanky<=225))||((tanky<=525)&&(tanky>=400))))||(((tankx==375)||(tankx==525))&&(((tanky<=525)&&(tanky>=425))||((tanky>=125)&&(tanky<=300))))||(((tankx==400)||(tankx==500))&&((tanky==225)||(tanky==300)||(tanky==425)))||(((tankx==425)||(tankx==475))&&((tanky!=250)&&(tanky!=275)&&(tanky!=350)&&(tanky!=375)&&(tanky>=125)))||((tankx==450)&&((tanky==425)||(tanky==400)||(tanky==325)||(tanky==300)||(tanky==225)))))){
                     tanky -= 25;
                     bufferGraphics.setColor(new Color(0x000000));
                     bufferGraphics.fillRect(tankx, tanky, 25, 25);
                     ((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
                 }
-
             }
             if((f == KeyEvent.VK_A) && (game)) {
-                tankx -= 25;
-                bufferGraphics.setColor(new Color(0x000000));
-                bufferGraphics.fillRect(tankx, tanky, 25, 25);
-                ((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                if(((tanky==100)&&((tankx>=150)&&(tankx<=775)))||(((tanky==175)&&(((tankx>=150)&&(tankx<=375))||(((tankx>=550)&&(tankx<=775)))))||(((tanky==200)||(tanky==225)||(tanky==275)||(tanky==400)||(tanky==425)||(tanky==525))&&((tankx>=150)&&(tankx<=775))))||((tanky==250)&&((tankx==150)||(tankx==175)||(tankx==350)||(tankx==375)||(tankx==550)||(tankx==575)||(tankx==750)||(tankx==775)))||((tanky==375)&&(((tankx>=150)&&(tankx<=350))||((tankx>=575)&&(tankx<=775))||(tankx==450)||(tankx==475)))||((tanky==300)&&((tankx==150)||((tankx>=350)&&(tankx<=575))||(tankx==775)))||((tanky==325)&&((tankx==150)||(tankx==350)||(tankx==450)||(tankx==475)||(tankx==575)||(tankx==775)))||((tanky==350)&&((tankx==150)||(tankx==350)||(tankx==575)||(tankx==775)))){
+                    tankx -= 25;
+                    bufferGraphics.setColor(new Color(0x000000));
+                    bufferGraphics.fillRect(tankx, tanky, 25, 25);
+                    ((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                }
             }
             if((f == KeyEvent.VK_S) && (game)) {
                 tanky += 25;
@@ -148,10 +149,12 @@ public abstract class Display {
                 ((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             }
             if((f == KeyEvent.VK_D) && (game)) {
-                tankx += 25;
-                bufferGraphics.setColor(new Color(0x000000));
-                bufferGraphics.fillRect(tankx, tanky, 25, 25);
-                ((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                if(((tanky==100)&&((tankx>=125)&&(tankx<=750)))||(((tanky==175)&&(((tankx>=125)&&(tankx<=350))||(((tankx>=525)&&(tankx<=750)))))||(((tanky==200)||(tanky==225)||(tanky==275)||(tanky==400)||(tanky==425)||(tanky==525))&&((tankx>=125)&&(tankx<=750))))||((tanky==250)&&((tankx==125)||(tankx==150)||(tankx==325)||(tankx==350)||(tankx==525)||(tankx==550)||(tankx==725)||(tankx==750)))||((tanky==375)&&(((tankx>=125)&&(tankx<=300))||((tankx>=550)&&(tankx<=750))||(tankx==425)||(tankx==450)))||((tanky==300)&&((tankx==125)||((tankx>=325)&&(tankx<=550))||(tankx==750)))||((tanky==325)&&((tankx==125)||(tankx==325)||(tankx==425)||(tankx==450)||(tankx==550)||(tankx==750)))||((tanky==350)&&((tankx==125)||(tankx==325)||(tankx==550)||(tankx==750)))){
+                    tankx += 25;
+                    bufferGraphics.setColor(new Color(0x000000));
+                    bufferGraphics.fillRect(tankx, tanky, 25, 25);
+                    ((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                }
             }
         }
 
