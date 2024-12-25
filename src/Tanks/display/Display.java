7180bdebdcf595d44b0949a1079ecb0a6468594e
HHOOSSTT <milovanarsen@gmail.com>
2024-12-25 21:43:46 +0300

    разворот картинки при определённом направлении движения

--- src/Tanks/display/Display.java
+++ src/Tanks/display/Display.java
@@ -12,7 +12,11 @@ import java.io.IOException;
 import java.util.Arrays;
 public abstract class Display {
 
-    private static Image imageTank;
+    private static Image imageTankUP;
+    private static Image imageTankDOWN;
+    private static Image imageTankLEFT;
+    private static Image imageTankRIGHT;
+    public static Image imageTankNOW;
 
     private static boolean created = false;
     private static JFrame window;
@@ -24,8 +28,11 @@ public abstract class Display {
     private static BufferStrategy bufferStrategy;
 
     public static void create(int length, int width, String title, int _clearColor, int numBuffers) throws IOException {
-        imageTank =   ImageIO.read(new File("tank.png"));
-
+        imageTankUP = ImageIO.read(new File("C:\\Users\\User\\Documents\\ProjectInformatika\\tankUP.png"));
+        imageTankDOWN = ImageIO.read(new File("C:\\Users\\User\\Documents\\ProjectInformatika\\tankDOWN.png"));
+        imageTankLEFT = ImageIO.read(new File("C:\\Users\\User\\Documents\\ProjectInformatika\\tankLEFT.png"));
+        imageTankRIGHT = ImageIO.read(new File("C:\\Users\\User\\Documents\\ProjectInformatika\\tankRIGHT.png"));
+        imageTankNOW = imageTankUP;
 
         if (created) {
             return;
@@ -118,9 +125,7 @@ public abstract class Display {
     public static int tankx = 775;
     public static int tanky = 525;
     public static void paintComponent5() {
-        //bufferGraphics.setColor(new Color(0x000000));
-        //bufferGraphics.fillRect(tankx, tanky, 25, 25);
-        bufferGraphics.drawImage(imageTank, tankx, tanky, 25, 25,  null);
+        bufferGraphics.drawImage(imageTankNOW, tankx, tanky, 25, 25,  null);
         ((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
     }
 
@@ -138,32 +143,32 @@ public abstract class Display {
             if((f == KeyEvent.VK_W) && (game)) {
                 if(((((tankx==775)||(tankx==125)||(tankx==325)||(tankx==575))&&((tanky>=125)&&(tanky<=525))||(((tankx==150)||(tankx==350)||(tankx==550)||(tankx==750))&&((tanky>175)&&(tanky<=425)))||(((tankx==175)||(tankx==725))&&(((tanky<=525)&&(tanky>=400))||((tanky>=125)&&(tanky<=275))))||(((tankx==200)||(tankx==250)||(tankx==300)||(tankx==600)||(tankx==650)||(tankx==700))&&(((tanky>=200)&&(tanky<=225))||((tanky<=475)&&(tanky>=400))))||(((tankx==225)||(tankx==275)||(tankx==625)||(tankx==675))&&(((tanky>=125)&&(tanky<=225))||((tanky<=525)&&(tanky>=400))))||(((tankx==375)||(tankx==525))&&(((tanky<=525)&&(tanky>=425))||((tanky>=125)&&(tanky<=300))))||(((tankx==400)||(tankx==500))&&((tanky==225)||(tanky==300)||(tanky==425)))||(((tankx==425)||(tankx==475))&&((tanky!=250)&&(tanky!=275)&&(tanky!=350)&&(tanky!=375)&&(tanky>=125)))||((tankx==450)&&((tanky==425)||(tanky==400)||(tanky==325)||(tanky==300)||(tanky==225)))))) {
                     tanky -= 25;
-                    bufferGraphics.setColor(new Color(0x000000));
-                    bufferGraphics.fillRect(tankx, tanky, 25, 25);
+                    imageTankNOW = imageTankUP;
+                    bufferGraphics.drawImage(imageTankNOW, tankx, tanky, 25, 25,  null);
                     ((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
                 }
             }
             if((f == KeyEvent.VK_A) && (game)) {
                 if(((tanky==100)&&((tankx>=150)&&(tankx<=775)))||(((tanky==175)&&(((tankx>=150)&&(tankx<=375))||(((tankx>=550)&&(tankx<=775)))))||(((tanky==200)||(tanky==225)||(tanky==275)||(tanky==400)||(tanky==425)||(tanky==525))&&((tankx>=150)&&(tankx<=775))))||((tanky==250)&&((tankx==150)||(tankx==175)||(tankx==350)||(tankx==375)||(tankx==550)||(tankx==575)||(tankx==750)||(tankx==775)))||((tanky==375)&&(((tankx>=150)&&(tankx<=350))||((tankx>=575)&&(tankx<=775))||(tankx==450)||(tankx==475)))||((tanky==300)&&((tankx==150)||((tankx>=350)&&(tankx<=575))||(tankx==775)))||((tanky==325)&&((tankx==150)||(tankx==350)||(tankx==450)||(tankx==475)||(tankx==575)||(tankx==775)))||((tanky==350)&&((tankx==150)||(tankx==350)||(tankx==575)||(tankx==775)))) {
                     tankx -= 25;
-                    bufferGraphics.setColor(new Color(0x000000));
-                    bufferGraphics.fillRect(tankx, tanky, 25, 25);
+                    imageTankNOW = imageTankLEFT;
+                    bufferGraphics.drawImage(imageTankNOW, tankx, tanky, 25, 25,  null);
                     ((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
                 }
             }
             if((f == KeyEvent.VK_S) && (game)) {
                 if((((tanky==100)||(tanky==125)||(tanky==150)||(tanky==425)||(tanky==450)||(tanky==475)||(tanky==500))&&((tankx==125)||(tankx==175)||(tankx==225)||(tankx==275)||(tankx==325)||(tankx==375)||(tankx==425)||(tankx==475)||(tankx==525)||(tankx==575)||(tankx==625)||(tankx==675)||(tankx==725)||(tankx==775)))||((tanky==175)&&((tankx!=400)&&(tankx!=450)&&(tankx!=500)))||((tanky==200)||(tanky==400))||(((tanky==225)||(tanky==250))&&((tankx==125)||(tankx==150)||(tankx==175)||(tankx==325)||(tankx==350)||(tankx==375)||(tankx==525)||(tankx==550)||(tankx==575)||(tankx==725)||(tankx==750)||(tankx==775)))||((tanky==275)&&((tankx!=175)&&(tankx!=200)&&(tankx!=225)&&(tankx!=250)&&(tankx!=275)&&(tankx!=300)&&(tankx!=600)&&(tankx!=625)&&(tankx!=650)&&(tankx!=675)&&(tankx!=700)&&(tankx!=725)))||(((tanky==300)&&((tankx==125)||(tankx==150)||(tankx==325)||(tankx==350)||(tankx==425)||(tankx==450)||(tankx==475)||(tankx==550)||(tankx==575)||(tankx==750)||(tankx==775))))||(((tanky==325)||(tanky==350))&&((tankx==125)||(tankx==150)||(tankx==325)||(tankx==350)||(tankx==550)||(tankx==575)||(tankx==750)||(tankx==775)))||((tanky==375)&&((tankx!=375)&&(tankx!=400)&&(tankx!=500)&&(tankx!=525)))) {
                     tanky += 25;
-                    bufferGraphics.setColor(new Color(0x000000));
-                    bufferGraphics.fillRect(tankx, tanky, 25, 25);
+                    imageTankNOW = imageTankDOWN;
+                    bufferGraphics.drawImage(imageTankNOW, tankx, tanky, 25, 25,  null);
                     ((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
                 }
             }
             if((f == KeyEvent.VK_D) && (game)) {
                 if(((tanky==100)&&((tankx>=125)&&(tankx<=750)))||(((tanky==175)&&(((tankx>=125)&&(tankx<=350))||(((tankx>=525)&&(tankx<=750)))))||(((tanky==200)||(tanky==225)||(tanky==275)||(tanky==400)||(tanky==425)||(tanky==525))&&((tankx>=125)&&(tankx<=750))))||((tanky==250)&&((tankx==125)||(tankx==150)||(tankx==325)||(tankx==350)||(tankx==525)||(tankx==550)||(tankx==725)||(tankx==750)))||((tanky==375)&&(((tankx>=125)&&(tankx<=300))||((tankx>=550)&&(tankx<=750))||(tankx==425)||(tankx==450)))||((tanky==300)&&((tankx==125)||((tankx>=325)&&(tankx<=550))||(tankx==750)))||((tanky==325)&&((tankx==125)||(tankx==325)||(tankx==425)||(tankx==450)||(tankx==550)||(tankx==750)))||((tanky==350)&&((tankx==125)||(tankx==325)||(tankx==550)||(tankx==750)))) {
                     tankx += 25;
-                    bufferGraphics.setColor(new Color(0x000000));
-                    bufferGraphics.fillRect(tankx, tanky, 25, 25);
+                    imageTankNOW = imageTankRIGHT;
+                    bufferGraphics.drawImage(imageTankNOW, tankx, tanky, 25, 25,  null);
                     ((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
                 }
             }
