aeb5d4e496eb9da64d1c1217ba1a2f1d175ecaf3
HHOOSSTT <milovanarsen@gmail.com>
2025-02-09 17:28:17 +0300

    добавление карты

--- src/Tanks/main/Main.java
+++ src/Tanks/main/Main.java
@@ -1,23 +1,26 @@
 package Tanks.main;
-import Tanks.display.Map;
-import Tanks.display.Tank;
 
-import java.awt.*;
+import Tanks.display.Map;
 import java.io.IOException;
+import java.util.Scanner;
 import javax.swing.*;
 
 public class Main{
     public static void main(String[] args) throws IOException {
-        JFrame jframe=new JFrame("Tanks");
-        jframe.setSize(950,700);
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jframe.setBackground(Color.GRAY);
-        jframe.setLocationRelativeTo(null);
-        jframe.setVisible(true);
-        Map map=new Map();
-        jframe.add(map);
-        Tank tank=new Tank(775,550,"UP");
-        jframe.add(tank);
-    }
+        Scanner sc=new Scanner(System.in);
+        JFrame frame = new JFrame("Tanks");
+        Map map=null;
+        try{
+            map=new Map();
+        }catch(IOException e){
+            System.err.println("Ошибка при создании карты: ");
+            return;
+        }
+        frame.add(map);
+        frame.setSize(950, 700);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+   }
 }
 
