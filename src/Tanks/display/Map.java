625d48e088ede140343df9b915e5db139dd232eb
HHOOSSTT <milovanarsen@gmail.com>
2025-04-04 07:38:18 +0300

    получение бонусов и окно победы и поражения

--- src/Tanks/display/Map.java
+++ src/Tanks/display/Map.java
@@ -9,15 +9,17 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Map extends JPanel implements KeyListener {
 
     public Tank tank1 = new Tank(175, 525, "UP");
     public Tank tank2 = new Tank(775, 525, "UP");
-    public TankBot tankbot1 = new TankBot(775, 100, "LEFT");
-    public TankBot tankbot2 = new TankBot(125, 200, "DOWN");
-    public TankBot tankbot3 = new TankBot(350, 275, "RIGHT");
     private final ArrayList<Bullet> bullets = new ArrayList<>();
+    private final ArrayList<Bullet> botbullets = new ArrayList<>();
+    public TankBot tankbot1 = new TankBot(775, 100, "LEFT", botbullets);
+    public TankBot tankbot2 = new TankBot(125, 175, "DOWN", botbullets);
+    public TankBot tankbot3 = new TankBot(350, 275, "RIGHT", botbullets);
     private boolean spacePressed = false;
     private boolean wPressed = false;
     private boolean aPressed = false;
@@ -27,13 +29,21 @@ public class Map extends JPanel implements KeyListener {
     private boolean leftPressed = false;
     private boolean downPressed = false;
     private boolean rightPressed = false;
-    private boolean shiftPressed = false;
+    private boolean pPressed = false;
     private final ArrayList<TankBot> tankBots = new ArrayList<>(Arrays.asList(tankbot1, tankbot2, tankbot3));
     private int score = 0;
     private int lifes1 = 3;
+    public int countbullets1 = 5;
+    public int countbullets2 = 5;
     private int lifes2 = 3;
     private boolean isGame = true;
     private final Timer game;
+    private final Timer bulletrecovery;
+    Random random = new Random();
+    public Bonus bonus1 = new Bonus(500, 400, ImageIO.read(new File("bonus.png")), random.nextInt(1, 4));
+    public Bonus bonus2 = new Bonus(200, 200, ImageIO.read(new File("bonus.png")), random.nextInt(1, 4));
+    private final ArrayList<Bonus> bonuses = new ArrayList<>(Arrays.asList(bonus1, bonus2));
+    private Timer respawnbonus;
 
     public Map() throws IOException {
         setFocusable(true);
@@ -47,6 +57,27 @@ public class Map extends JPanel implements KeyListener {
             repaint();
         });
         game.start();
+        bulletrecovery = new Timer(1500, e->{
+            if(countbullets1 < 5){
+                countbullets1++;
+            }
+            if(countbullets2 < 5){
+                countbullets2++;
+            }
+            repaint();
+        });
+        bulletrecovery.start();
+        for(Bonus bonus : bonuses){
+            respawnbonus = new Timer(23000, e-> {
+                try {
+                    bonus.respawn(bonus.getBonusX(), bonus.getBonusY(), ImageIO.read(new File("bonus.png")));
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+                repaint();
+            });
+            respawnbonus.start();
+        }
     }
 
     @Override
@@ -56,10 +87,14 @@ public class Map extends JPanel implements KeyListener {
         g.setFont(new Font("Arial", Font.BOLD, 30));
         String s1 = "Score:" + score;
         g.drawString(s1, 100, 50);
-        String s2 = "Lifes player1:" + lifes1;
+        String s2 = "Player1 lifes:" + lifes1;
         g.drawString(s2, 320, 50);
-        String s3 = "Lifes player2:" + lifes2;
+        String s3 = "Player2 lifes:" + lifes2;
         g.drawString(s3, 610, 50);
+        String s4 = "Player1 bullets:" + countbullets1;
+        g.drawString(s4, 20, 620);
+        String s5 = "Player2 bullets:" + countbullets2;
+        g.drawString(s5, 670, 620);
         Image imageStenka = null;
         try {
             imageStenka = ImageIO.read(new File("stenka.png"));
@@ -111,7 +146,6 @@ public class Map extends JPanel implements KeyListener {
             g.drawLine(j, 100, j, 550);
             ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         }
-
         int a = 150;
         int i = 1;
         while (a <= 750) {
@@ -145,8 +179,14 @@ public class Map extends JPanel implements KeyListener {
         for (Bullet bullet : bullets) {
             bullet.paintComponent(g);
         }
+        for (Bullet bullet : botbullets) {
+            bullet.paintComponent(g);
+        }
         for (TankBot tankbot : tankBots){
             tankbot.paintComponent(g);
+        }
+        for(Bonus bonus : bonuses){
+            bonus.paintComponent(g);
         }
     }
 
@@ -156,10 +196,38 @@ public class Map extends JPanel implements KeyListener {
         tankbot2.stopBotTimer();
         tankbot3.stopBotTimer();
         game.stop();
+        bulletrecovery.stop();
+    }
+
+    private void Victory() {
+        SwingUtilities.invokeLater(() -> {
+            JOptionPane.showMessageDialog(
+                    this,
+                    "Игроки победили!",
+                    "Победа",
+                    JOptionPane.INFORMATION_MESSAGE
+            );
+        });
+    }
+
+    private void Lose() {
+        SwingUtilities.invokeLater(() -> {
+            JOptionPane.showMessageDialog(
+                    this,
+                    "Игроки проиграли!",
+                    "Поражение",
+                    JOptionPane.INFORMATION_MESSAGE
+            );
+        });
     }
 
     private void updateGame() throws IOException {
+        if((score == 10) && (isGame)){
+            Victory();
+            stopGame();
+        }
         if((lifes1 <= 0) && (lifes2 <= 0)) {
+            Lose();
             stopGame();
         }
         if(lifes1 == 0){
@@ -171,31 +239,33 @@ public class Map extends JPanel implements KeyListener {
         for (Bullet bullet : bullets) {
             bullet.move();
         }
-        for (TankBot tankbot : tankBots){
-            if (!tankbot.isAlive()) continue;
-            tankbot.updateBullets();
-            bullets.removeIf(bullet -> {
-                if(tankbot.isHit(bullet.getBulletX(), bullet.getBulletY())) {
-                    if(!tankbot.isAlive()) {
-                        score ++;
-                    }
-                    return true;
+        bullets.removeIf(b -> !b.isCanMove());
+        botbullets.removeIf(b -> !b.isCanMove());
+        for (Bullet bullet : botbullets) {
+            bullet.move();
+        }
+        botbullets.removeIf(b -> !b.isCanMove());
+        for(TankBot tankbot : tankBots){
+            for(Bullet bullet : bullets){
+                if ((bullet.ishitTank(bullet.getBulletX(), bullet.getBulletY(), tankbot.getTankbotX(), tankbot.getTankbotY())) && (tankbot.isAlive())){
+                    score ++;
+                    tankbot.setisAlive(false);
+                    bullet.setcanMove(false);
                 }
-                return !bullet.isCanMove();
-            });
+            }
         }
         for (TankBot tankbot : tankBots){
             if (!tankbot.isAlive()) continue;
-            for (Bullet bullet : tankbot.getBotBullets().getBullets()) {
-                if (bullet.ishitTank(bullet.getBulletX(), bullet.getBulletY(), tank1.getTankx(), tank1.getTanky())) {
+            for (Bullet bullet : botbullets) {
+                if ((bullet.ishitTank(bullet.getBulletX(), bullet.getBulletY(), tank1.getTankx(), tank1.getTanky())) && (!tank1.getInvisible())){
                     lifes1--;
-                    tankbot.getBotBullets().getBullets().remove(bullet);
+                    botbullets.remove(bullet);
                     tank1.returnAfterHit1();
                     break;
                 }
-                if (bullet.ishitTank(bullet.getBulletX(), bullet.getBulletY(), tank2.getTankx(), tank2.getTanky())) {
+                if ((bullet.ishitTank(bullet.getBulletX(), bullet.getBulletY(), tank2.getTankx(), tank2.getTanky())) && (!tank2.getInvisible())){
                     lifes2--;
-                    tankbot.getBotBullets().getBullets().remove(bullet);
+                    botbullets.remove(bullet);
                     tank2.returnAfterHit2();
                     break;
                 }
@@ -213,6 +283,32 @@ public class Map extends JPanel implements KeyListener {
         if(!tankbot3.isAlive()){
             tankbot3.returnAfterHit(350, 275, "RIGHT");
         }
+        for(Bonus bonus : bonuses){
+            if((tank1.getTankx() == bonus.getBonusX()) &&  (tank1.getTanky() == bonus.getBonusY()) && (bonus.isVisible())){
+                if(bonus.getBonuscode() == 1){
+                    lifes1++;
+                }
+                if(bonus.getBonuscode() == 2){
+                    countbullets1+=5;
+                }
+                if(bonus.getBonuscode() == 3){
+                    tank1.setInvisible(true);
+                }
+                bonus.setImage(null);
+            }
+            if((tank2.getTankx() == bonus.getBonusX()) &&  (tank2.getTanky() == bonus.getBonusY()) && (bonus.isVisible())){
+                if(bonus.getBonuscode() == 1){
+                    lifes2++;
+                }
+                if(bonus.getBonuscode() == 2){
+                    countbullets2+=5;
+                }
+                if(bonus.getBonuscode() == 3){
+                    tank2.setInvisible(true);
+                }
+                bonus.setImage(null);
+            }
+        }
     }
 
     @Override
@@ -221,90 +317,82 @@ public class Map extends JPanel implements KeyListener {
     public void keyPressed(KeyEvent e) {
         int f = e.getKeyCode();
         if ((f == KeyEvent.VK_W) && (!wPressed) && (isGame) && (lifes1 !=0)) {
-            if(!tank1.botisUP()){
-                tank1.moveUp(tank1.imagetankNOW.getGraphics());
-            }
+            tank1.moveUp(tank1.imagetankNOW.getGraphics());
             wPressed = true;
         }
         if ((f == KeyEvent.VK_UP) && (!upPressed) && (isGame) && (lifes2 != 0)) {
-            if(!tank2.botisUP()){
-                tank2.moveUp(tank2.imagetankNOW.getGraphics());
-            }
+            tank2.moveUp(tank2.imagetankNOW.getGraphics());
             upPressed = true;
         }
         if ((f == KeyEvent.VK_A) && (!aPressed) && (isGame) && (lifes1 != 0)) {
-            if(!tank1.botisLEFT()){
-                tank1.moveLEFT(tank1.imagetankNOW.getGraphics());
-            }
+            tank1.moveLEFT(tank1.imagetankNOW.getGraphics());
             aPressed = true;
         }
         if ((f == KeyEvent.VK_LEFT) && (!leftPressed) && (isGame) && (lifes2 != 0)) {
-            if(!tank2.botisLEFT()){
-                tank2.moveLEFT(tank2.imagetankNOW.getGraphics());
-            }
+            tank2.moveLEFT(tank2.imagetankNOW.getGraphics());
             leftPressed = true;
         }
         if ((f == KeyEvent.VK_S) && (!sPressed) && (isGame) && (lifes1 != 0)) {
-            if(!tank1.botisDOWN()){
-                tank1.moveDOWN(tank1.imagetankNOW.getGraphics());
-            }
+            tank1.moveDOWN(tank1.imagetankNOW.getGraphics());
             sPressed = true;
         }
         if ((f == KeyEvent.VK_DOWN) && (!downPressed) && (isGame) && (lifes2 != 0)) {
-            if(!tank2.botisDOWN()){
-                tank2.moveDOWN(tank2.imagetankNOW.getGraphics());
-            }
+            tank2.moveDOWN(tank2.imagetankNOW.getGraphics());
             downPressed = true;
         }
         if ((f == KeyEvent.VK_D) && (!dPressed) && (isGame) && (lifes1 != 0)) {
-            if(!tank1.botisRIGHT()){
-                tank1.moveRIGHT(tank1.imagetankNOW.getGraphics());
-            }
+            tank1.moveRIGHT(tank1.imagetankNOW.getGraphics());
             dPressed = true;
         }
         if ((f == KeyEvent.VK_RIGHT) && (!rightPressed) && (isGame) && (lifes2 != 0)) {
-            if(!tank2.botisRIGHT()){
-                tank2.moveRIGHT(tank2.imagetankNOW.getGraphics());
-            }
+            tank2.moveRIGHT(tank2.imagetankNOW.getGraphics());
             rightPressed = true;
         }
         if ((f == KeyEvent.VK_SPACE) && (!spacePressed) && (isGame) && (lifes1 != 0)) {
-            if (tank1.getDirection().equals("UP")) {
+            if ((tank1.getDirection().equals("UP")) && (countbullets1 > 0)) {
                 Bullet bullet = new Bullet(tank1.getTankx() + 8, tank1.getTanky() - 5, "UP");
                 bullets.add(bullet);
+                countbullets1--;
             }
-            if (tank1.getDirection().equals("LEFT")) {
+            if ((tank1.getDirection().equals("LEFT")) && (countbullets1 > 0)) {
                 Bullet bullet = new Bullet(tank1.getTankx() - 5, tank1.getTanky() + 8, "LEFT");
                 bullets.add(bullet);
+                countbullets1--;
             }
-            if (tank1.getDirection().equals("DOWN")) {
+            if ((tank1.getDirection().equals("DOWN")) && (countbullets1 > 0)) {
                 Bullet bullet = new Bullet(tank1.getTankx() + 8, tank1.getTanky() + 20, "DOWN");
                 bullets.add(bullet);
+                countbullets1--;
             }
-            if (tank1.getDirection().equals("RIGHT")) {
+            if ((tank1.getDirection().equals("RIGHT")) && (countbullets1 > 0)) {
                 Bullet bullet = new Bullet(tank1.getTankx() + 20, tank1.getTanky() + 8, "RIGHT");
                 bullets.add(bullet);
+                countbullets1--;
             }
             spacePressed = true;
         }
-        if ((f == KeyEvent.VK_SHIFT) && (!shiftPressed) && (isGame) && (lifes2 != 0)) {
-            if (tank2.getDirection().equals("UP")) {
+        if ((f == KeyEvent.VK_P) && (!pPressed) && (isGame) && (lifes2 != 0)) {
+            if ((tank2.getDirection().equals("UP")) && (countbullets2 > 0)) {
                 Bullet bullet = new Bullet(tank2.getTankx() + 8, tank2.getTanky() - 5, "UP");
                 bullets.add(bullet);
+                countbullets2--;
             }
-            if (tank2.getDirection().equals("LEFT")) {
+            if ((tank2.getDirection().equals("LEFT")) && (countbullets2 > 0)) {
                 Bullet bullet = new Bullet(tank2.getTankx() - 5, tank2.getTanky() + 8, "LEFT");
                 bullets.add(bullet);
+                countbullets2--;
             }
-            if (tank2.getDirection().equals("DOWN")) {
+            if ((tank2.getDirection().equals("DOWN")) && (countbullets2 > 0)) {
                 Bullet bullet = new Bullet(tank2.getTankx() + 8, tank2.getTanky() + 20, "DOWN");
                 bullets.add(bullet);
+                countbullets2--;
             }
-            if (tank2.getDirection().equals("RIGHT")) {
+            if ((tank2.getDirection().equals("RIGHT")) && (countbullets2 > 0)) {
                 Bullet bullet = new Bullet(tank2.getTankx() + 20, tank2.getTanky() + 8, "RIGHT");
                 bullets.add(bullet);
+                countbullets2--;
             }
-            shiftPressed = true;
+            pPressed = true;
         }
         repaint();
     }
@@ -319,6 +407,6 @@ public class Map extends JPanel implements KeyListener {
         leftPressed = false;
         downPressed = false;
         rightPressed = false;
-        shiftPressed = false;
+        pPressed = false;
     }
 }
